a6cb197c6dae3e65fab73fc3bc0010bdec4cd26c
migueel15 <miguelangeldorado10@gmail.com>
2025-04-13 22:00:04 +0200

    game type

--- src/types/game.ts
+++ src/types/game.ts
@@ -3,7 +3,7 @@ export type Card = {
   color: "red" | "blue" | "black" | "empty";
   isSelected: boolean;
   isFlipped: boolean;
-  handleClickCard?: (word: string) => void;
+  // handleClickCard?: (word: string) => void;
 };
 
 export type Board = {
@@ -11,12 +11,30 @@ export type Board = {
 };
 
 export type User = {
+  id: string;
   name: string;
 };
 
 export type GameState = {
   code: string;
-  blueTeam: User[];
-  redTeam: User[];
+  players: User[]; // para jugadores aun no asignados a equipos en el lobby
+  teams: {
+    blue: {
+      leader: User;
+      agents: User[];
+    };
+    red: {
+      leader: User;
+      agents: User[];
+    };
+  };
   cards: Card[];
+  turn: {
+    team: "red" | "blue";
+    role: "leader" | "agent";
+  };
+  clue: {
+    word: string;
+    count: number;
+  } | null;
 };
