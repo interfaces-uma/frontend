b7ae573e9bfc6ebff948ba4aaed531a9e66092a7
migueel15 <miguelangeldorado10@gmail.com>
2025-04-14 00:27:29 +0200

    add: feature game

---
+++ src/features/game/Game.tsx
@@ -0,0 +1,57 @@
+import Board from "@/components/Board";
+import type { Card } from "@/types/game";
+import { useEffect } from "react";
+import { useBoardManager } from "./hooks/useBoardManager";
+export default function Game() {
+  const { board, handleCardClick, addCards } = useBoardManager();
+
+  // export type Card = {
+  //   word: string;
+  //   color: "red" | "blue" | "black" | "empty";
+  //   isSelected: boolean;
+  //   isFlipped: boolean;
+  //   // handleClickCard?: (word: string) => void;
+  // };
+  const cards: Card[] = [
+    { word: "apple", color: "red", isSelected: false, isFlipped: false },
+    { word: "banana", color: "blue", isSelected: false, isFlipped: false },
+    { word: "cherry", color: "black", isSelected: false, isFlipped: false },
+    { word: "date", color: "empty", isSelected: false, isFlipped: false },
+    { word: "fig", color: "red", isSelected: false, isFlipped: false },
+    { word: "grape", color: "blue", isSelected: false, isFlipped: false },
+    { word: "kiwi", color: "black", isSelected: false, isFlipped: false },
+    { word: "lemon", color: "empty", isSelected: false, isFlipped: false },
+    { word: "mango", color: "red", isSelected: false, isFlipped: false },
+    { word: "nectarine", color: "blue", isSelected: false, isFlipped: false },
+    { word: "orange", color: "black", isSelected: false, isFlipped: false },
+    { word: "papaya", color: "empty", isSelected: false, isFlipped: false },
+    { word: "quince", color: "red", isSelected: false, isFlipped: false },
+    { word: "raspberry", color: "blue", isSelected: false, isFlipped: false },
+    { word: "strawberry", color: "black", isSelected: false, isFlipped: false },
+    { word: "tangerine", color: "empty", isSelected: false, isFlipped: false },
+    { word: "ugli fruit", color: "red", isSelected: false, isFlipped: false },
+    {
+      word: "vanilla bean",
+      color: "blue",
+      isSelected: false,
+      isFlipped: false,
+    },
+    { word: "watermelon", color: "black", isSelected: false, isFlipped: false },
+    { word: "xigua", color: "empty", isSelected: false, isFlipped: false },
+    {
+      word: "yellow passion fruit",
+      color: "red",
+      isSelected: false,
+      isFlipped: false,
+    },
+    { word: "zucchini", color: "blue", isSelected: false, isFlipped: false },
+    { word: "apricot", color: "black", isSelected: false, isFlipped: false },
+    { word: "blueberry", color: "empty", isSelected: false, isFlipped: false },
+    { word: "fiaaag", color: "empty", isSelected: false, isFlipped: false },
+  ];
+  useEffect(() => {
+    addCards(cards);
+  }, []);
+
+  return <Board board={board} handleCardClick={handleCardClick} />;
+}
---
+++ src/features/game/hooks/useBoardManager.ts
@@ -0,0 +1,64 @@
+import type { Board, Card } from "@/types/game";
+import { useState } from "react";
+
+export function useBoardManager() {
+  const [board, setBoard] = useState<Board>({ cards: [] });
+
+  /**
+   * Añade la carta al board
+   * @param card - Carta a añadir
+   */
+  const addCard = (card: Card) => {
+    setBoard((prev) => ({
+      ...prev,
+      cards: [...prev.cards, card],
+    }));
+  };
+
+  /**
+   * Añade la lista de cartas al board
+   * @param newCards: lista de cartas a añadir
+   */
+  const addCards = (newCards: Card[]) => {
+    setBoard((prev) => ({
+      ...prev,
+      cards: [...prev.cards, ...newCards],
+    }));
+  };
+
+  /**
+   * Pone el atributo isFlipped de la carta a true.
+   * @param cardWord: palabra de la carta que quieras levantar
+   */
+  const showCard = (cardWord: Card["word"]) => {
+    setBoard((prev) => ({
+      ...prev,
+      cards: prev.cards.map((card) =>
+        card.word === cardWord ? { ...card, isFlipped: true } : card,
+      ),
+    }));
+  };
+
+  /**
+   * Pone el atributo isSelected de la carta a true.
+   * @param cardWord - Carta sobre la que se hace click
+   */
+  const handleCardClick = (cardWord: Card["word"]) => {
+    setBoard((prev) => ({
+      ...prev,
+      cards: prev.cards.map((card) =>
+        card.word === cardWord
+          ? { ...card, isSelected: !card.isSelected }
+          : card,
+      ),
+    }));
+  };
+
+  return {
+    board,
+    addCard,
+    addCards,
+    showCard,
+    handleCardClick,
+  };
+}
