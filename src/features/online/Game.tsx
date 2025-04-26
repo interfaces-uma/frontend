7d73614325bc246782a3156e754b9cd9d1df749e
migueel15 <miguelangeldorado10@gmail.com>
2025-04-26 12:22:58 +0200

    add cards from gameState

--- src/features/online/Game.tsx
+++ src/features/online/Game.tsx
@@ -2,54 +2,29 @@ import Board from "@/components/Board";
 import type { Card } from "@/types";
 import { useEffect } from "react";
 import { useBoardManager } from "./hooks/useBoardManager";
+import { socket } from "./service/socket";
+import { useOnlineManager } from "./hooks/useOnlineManager";
 export default function Game() {
   const { board, handleCardClick, addCards } = useBoardManager();
+  const { state, dispatch } = useOnlineManager();
 
-  const cards: Card[] = [
-    { word: "apple", color: "red", isSelected: false, isFlipped: false },
-    { word: "banana", color: "blue", isSelected: false, isFlipped: false },
-    { word: "cherry", color: "black", isSelected: false, isFlipped: false },
-    { word: "date", color: "empty", isSelected: false, isFlipped: false },
-    { word: "fig", color: "red", isSelected: false, isFlipped: false },
-    { word: "grape", color: "blue", isSelected: false, isFlipped: false },
-    { word: "kiwi", color: "black", isSelected: false, isFlipped: false },
-    { word: "lemon", color: "empty", isSelected: false, isFlipped: false },
-    { word: "mango", color: "red", isSelected: false, isFlipped: false },
-    { word: "nectarine", color: "blue", isSelected: false, isFlipped: false },
-    { word: "orange", color: "black", isSelected: false, isFlipped: false },
-    { word: "papaya", color: "empty", isSelected: false, isFlipped: false },
-    { word: "quince", color: "red", isSelected: false, isFlipped: false },
-    { word: "raspberry", color: "blue", isSelected: false, isFlipped: false },
-    { word: "strawberry", color: "black", isSelected: false, isFlipped: false },
-    { word: "tangerine", color: "empty", isSelected: false, isFlipped: false },
-    { word: "ugli fruit", color: "red", isSelected: false, isFlipped: false },
-    {
-      word: "vanilla bean",
-      color: "blue",
-      isSelected: false,
-      isFlipped: false,
-    },
-    { word: "watermelon", color: "black", isSelected: false, isFlipped: false },
-    { word: "xigua", color: "empty", isSelected: false, isFlipped: false },
-    {
-      word: "yellow passion fruit",
+  const pruebaBack = (cardWord: string) => {
+    const card: Card = {
+      word: cardWord,
       color: "red",
-      isSelected: false,
       isFlipped: false,
-    },
-    { word: "zucchini", color: "blue", isSelected: false, isFlipped: false },
-    { word: "apricot", color: "black", isSelected: false, isFlipped: false },
-    { word: "blueberry", color: "empty", isSelected: false, isFlipped: false },
-    { word: "fiaaag", color: "empty", isSelected: false, isFlipped: false },
-  ];
-  useEffect(() => {
-    addCards(cards);
-  }, []);
+      isSelected: false,
+    };
+    console.log(cardWord);
+    socket.emit("guessCard", card);
+  };
+
+  const cards = { cards: state.cards };
 
   return (
     <div className="flex w-max h-[100%]">
       <div className="board-section w-[70%]">
-        <Board board={board} handleCardClick={handleCardClick} />
+        <Board board={cards} handleCardClick={pruebaBack} />
       </div>
     </div>
   );
