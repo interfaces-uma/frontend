f87486d14a8210fca6d37ef73e149298971c6979
joseruuiz4 <joseruizpareja@uma.es>
2025-04-30 13:33:45 +0200

    prueba layout Game

--- src/features/online/Game.tsx
+++ src/features/online/Game.tsx
@@ -5,6 +5,8 @@ import Button from "@/components/Button";
 import BackIcon from "@/components/Icons/IconBack";
 import SettingsIcon from "@/components/Icons/IconSettings";
 import { useOnlineManager } from "./hooks/useOnlineManager";
+import { T } from "node_modules/react-router/dist/development/fog-of-war-1hWhK5ey.d.mts";
+import TeamInfo from "@/components/teamInfo";
 export default function Game() {
   const { state } = useGameState();
   const manager = useOnlineManager();
@@ -23,44 +25,39 @@ export default function Game() {
   const cards = { cards: state.cards };
 
   return (
-    <div className="h-screen w-screen grid grid-cols-[1fr_3fr_1fr] grid-rows-[1fr_1fr] gap-2 p-4 bg-fondo">
-      <div className="col-span-3 flex justify-between items-center">
-        <section
-          id="botonSalir"
-          className="absolute top-2 left-2 xl:top-7 xl:left-7 z-10"
-        >
-          <Button onClick={() => {}} circular>
-            <BackIcon fill="fondo" />
-          </Button>
-        </section>
+    <div className="h-screen w-screen flex flex-col bg-fondo">
+      <div className="flex justify-between h-[100px] px-4 bg-cartas">
+        <Button onClick={() => {}} circular inversed>
+          <BackIcon fill="currentColor" className="cartas" />
+        </Button>
 
-        <section className="absolute top-2 right-2 xl:top-7 xl:right-7 z-10">
-          <Button onClick={() => {}} circular>
-            <SettingsIcon
-              fill="currentColor"
-              className="cartas"
-              width={20}
-              height={20}
-            />
-          </Button>
-        </section>
-      </div>
-
-      <div className="bg-red-500 rounded-xl text-white p-4 ">Panel Rojo</div>
-
-      <div className="flex items-center justify-center w-full h-full row-span-2">
-        <div className="w-full max-w-[1000px] min-w-[650px]">
-          <Board
-            board={cards}
-            role={state.user.role || "spectator"}
-            handleCardClick={manager.revealCard}
+        <Button onClick={() => {}} circular inversed>
+          <SettingsIcon
+            fill="currentColor"
+            className="cartas"
+            width={20}
+            height={20}
           />
-        </div>
+        </Button>
       </div>
 
-      <div className="bg-blue-500 rounded-xl text-white p-4">Panel Azul</div>
-      <div className="col-start-3">
-        <Chat />
+      <div className="col-span-3 px-4 grid grid-cols-[1fr_3fr_1fr] grid-rows-[1fr_1fr] gap-2">
+        <TeamInfo team="red" />
+
+        <div className="flex  justify-center w-full h-full row-span-2">
+          <div className="w-full max-w-[1000px] min-w-[650px]">
+            <Board
+              board={cards}
+              role={state.user.role || "spectator"}
+              handleCardClick={manager.revealCard}
+            />
+          </div>
+        </div>
+
+        <TeamInfo team="blue" />
+        <div className="col-start-3">
+          <Chat />
+        </div>
       </div>
     </div>
   );
