588d4da9df719317ad6b8d527543b054b71461ed
saalma4 <160852812+saalma4@users.noreply.github.com>
2025-05-14 19:06:32 +0200

    tutorial FUNCIONA

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,18 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "@/style.css";
 import "./i18n";
 
+// Páginas
+import Home from "@/pages/Home";
+import Lobby from "@/pages/LobbyPage";
+import GamePage from "@/pages/GamePage";
+import TutorialPage from "@/pages/TutorialPage";
+
+// Contextos
+import { GameProvider } from "@/context/game/GameContext";
+import { VolumeProvider } from "@/context/backgroundVolume/backgroundVolumeContext";
+import { HoverVolumeProvider } from "@/context/hoverVolume/hoverVolumeContext";
+import { ClickVolumeProvider } from "@/context/clickVolume/clickVolumeContext";
+
 // Set font size from localStorage on initial load
 const savedFontIndex = localStorage.getItem("fontIndex");
 if (savedFontIndex) {
@@ -14,22 +26,12 @@ if (savedFontIndex) {
   );
 }
 
-// Páginas
-import Home from "@/pages/Home";
-import Lobby from "@/pages/LobbyPage";
-import GamePage from "@/pages/GamePage";
-
-// Contextos
-import { GameProvider } from "@/context/game/GameContext";
-import { VolumeProvider } from "@/context/backgroundVolume/backgroundVolumeContext";
-import { HoverVolumeProvider } from "@/context/hoverVolume/hoverVolumeContext";
-import { ClickVolumeProvider } from "@/context/clickVolume/clickVolumeContext";
-
 // Rutas
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/lobby", element: <Lobby /> },
   { path: "/game", element: <GamePage /> },
+  { path: "/tutorial", element: <TutorialPage /> },
 ]);
 
 // Renderizado principal con todos los contextos
