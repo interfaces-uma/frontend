afc8e53ea2a640722cef9a884a20c61fe6461d8d
Carlos Rodríguez <carlosrodriguezmartin2004@gmail.com>
2025-05-14 14:27:15 +0200

    fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,6 @@ import Button from "@/features/shared/components/Button";
 import SettingsIcon from "@/features/shared/components/Icons/IconSettings";
 import Popup from "@/features/shared/components/Popup";
 import { useState } from "react";
-<<<<<<< HEAD
-import BackgroundMusic from "@/components/BackgroundMusic";
 import { useTranslation } from "react-i18next";
 
 function Home() {
@@ -83,82 +81,6 @@ function Home() {
 				/>
 				<Popup autoWelcome />
 			</div>
-=======
-import { useNavigate } from "react-router";
-
-function Home() {
-  const [showSettings, setShowSettings] = useState(false);
-  const [showName, setShowName] = useState(false);
-  const [showNameCode, setShowNameCode] = useState(false);
-  const [isPopupOpen, setPopupOpen] = useState(false);
-  const navigate = useNavigate();
-  const openSettings = () => setShowSettings(!showSettings);
-  const openName = () => setShowName(!showName);
-  const openNameCode = () => {
-    setShowName(!showName);
-    setShowNameCode(!showNameCode);
-  };
-  const openPopup = () => setPopupOpen(!isPopupOpen);
-  return (
-    <>
-      {/* Vista movil */}
-      <div className="w-full h-full bg-fondo overflow-hidden md:hidden">
-        <div className="flex flex-col mx-[15%] justify-center items-center h-full">
-          <h1 className="text-center text-[clamp(3rem,5.5vw,100.5rem)]/12 w-full text-white mt-10">
-            CÓDIGO SECRETO
-          </h1>
-          <img
-            src={homePhoto}
-            alt="home"
-            className="aspect-square w-full mt-5 lg:w-[30%]"
-          />
-          <div id="botones" className="h-[25%] w-full mt-5 flex flex-col">
-            <Button onClick={openName} style="w-full h-[30%] m-auto">
-              CREAR MESA
-            </Button>
-            <Button onClick={openNameCode} style="w-full h-[30%] m-auto">
-              UNIRSE A MESA
-            </Button>
-            <Button
-              onClick={openPopup}
-              style="w-full h-[30%] m-auto"
-              narrator="TUTORIAL"
-            >
-              COMO JUGAR
-            </Button>
-          </div>
-          <div
-            id="opciones"
-            className="flex justify-center items-center gap-4 mt-5 mb-5 w-full"
-          >
-            <Button
-              narrator="Ajustes"
-              onClick={openSettings}
-              circular
-              style="w-13 h-13 flex items-center justify-center"
-            >
-              <SettingsIcon stroke="fondo" />
-            </Button>
-            <BackgroundMusic />
-          </div>
-        </div>
-        {showSettings && <Settings onClose={openSettings} />}
-        {showName && !showNameCode && <Name onClose={openName} />}
-        {showNameCode && <Name onClose={openNameCode} unirse />}
-        <Popup
-          isOpen={isPopupOpen}
-          onClose={openPopup}
-          message="APRENDE A JUGAR"
-        >
-          <Button
-            onClick={() => navigate("/tutorial")}
-            style="w-full bg-fondo text-white rounded-lg"
-          >
-            JUGAR TUTORIAL
-          </Button>
-        </Popup>
-      </div>
->>>>>>> 27d27cdd65bed3353a05188c0d7c1d92a15297da
 
 			{/* Vista pc */}
 			<div className="bg-fondo h-screen w-screen flex-col items-center justify-center hidden md:flex">
@@ -189,7 +111,6 @@ function Home() {
 				{showName && !showNameCode && <Name onClose={openName} />}
 				{showNameCode && <Name onClose={openNameCode} unirse />}
 
-<<<<<<< HEAD
 				{/* Tutorial popup manual */}
 				<Popup
 					isOpen={isPopupOpen}
@@ -202,27 +123,6 @@ function Home() {
 			</div>
 		</>
 	);
-=======
-        {/* Tutorial popup manual */}
-        <Popup
-          isOpen={isPopupOpen}
-          onClose={openPopup}
-          message="APRENDE A JUGAR"
-        >
-          <Button
-            onClick={() => navigate("/tutorial")}
-            style="w-full bg-fondo text-white rounded-lg"
-          >
-            JUGAR TUTORIAL
-          </Button>
-        </Popup>
-
-        {/* Auto-popup de bienvenida + explicación */}
-      </div>
-      <Popup autoWelcome />
-    </>
-  );
->>>>>>> 27d27cdd65bed3353a05188c0d7c1d92a15297da
 }
 
 export default Home;
